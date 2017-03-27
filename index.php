5bb2c575823560125a31f61ae046e6213cf275e5
Wwate <wateksi.94@hotmail.com>
2017-03-27 18:51:04 +0300

    Php files

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title of the document</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<form action="signup.php" method="POST">
+	<input type="text" name="first" placeholder="Etunimi"><br>
+	<input type="text" name="last" placeholder="Sukunimi"><br>
+	<input type="text" name="uid" placeholder="Käyttäjänimi"><br>
+	<input type="password" name="pwd" placeholder="Salasana"><br>
+	<button>REKISTERÖIDY</button>
+	
+</form>
+</body>
+</html>
